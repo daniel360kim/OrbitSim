1cbbf09d2045db41afcc678a5e0062a24b69c10b
daniel360kim <nmrs.thrust@gmail.com>
2023-07-01 15:38:29 -0600

    added imgui

--- Simulator/src/main.cpp
+++ Simulator/src/main.cpp
@@ -1,34 +1,39 @@
+#include <GLFW/glfw3.h>
 
+#include "backends/imgui_impl_glfw.h"
 
-#include <iostream>
-#include <iomanip>
-
-#include "objects/CentralBody.h"
-#include "objects/OrbitalObject.h"
-#include "orbit/OrbitalPropogator.h"
-
-
-int main(int argc, char const *argv[])
+int main(void)
 {
-    
-    CentralBody earth("earth", Type::Planet, 5.97219e24, 6371, 3);
-    OrbitalObject moon = OrbitalObjectBuilder("Moon", Type::Planet, 7.34767309e22)
-        .setSemiMajorAxis(384399)
-        .setEccentricity(0.0549)
-        .setInclination(5.145)
-        .setLongitudeOfAscendingNode(0)
-        .setCentralBody(earth)
-        .build();
+    GLFWwindow* window;
 
-    OrbitalPropogator propogator(moon, 1);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-    double period = propogator.getOrbitalPeriod();
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-    std::cout <<std::setprecision(10) << "Orbital Period: " << period << std::endl;
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-    propogator.propogateOrbit(period);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
-
 }
-
