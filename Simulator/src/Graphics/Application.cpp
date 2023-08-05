1ae5f69c06f2bce0bf49340192aba08aee2c742a
daniel360kim <nmrs.thrust@gmail.com>
2023-08-05 12:04:51 -0600

    removed unneeded console logging

--- Simulator/src/Graphics/Application.cpp
+++ Simulator/src/Graphics/Application.cpp
@@ -47,7 +47,6 @@ namespace Visualization
 
     void Application::OnUIRender()
     {
-        std::cout << "Current Scene: " << static_cast<int>(m_CurrentScene) << std::endl;
         switch (m_CurrentScene)
         {
         case SceneSelection::OpeningScene:
